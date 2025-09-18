025b952bec66f33f27e56763fd16d30cc61b561d
UstubecEmre <emresb1999@gmail.com>
2025-09-18 15:05:36 +0300

    fix(db): resolve foreign key issue and successfully insert sample data

--- src/sql_quiz.py
+++ src/sql_quiz.py
@@ -5,7 +5,10 @@ import os
 
 #%% create a new database
 
-db_path = "employees.db"
+db_folder = "db"
+os.makedirs(db_folder, exist_ok= True) 
+db_path = os.path.join(db_folder,"employees.db")
+
 def create_database():
     if os.path.exists(db_path):
         print("Database already exists. Connecting ...")
@@ -30,7 +33,7 @@ def create_table_employee(cursor):
                        CompanyID INTEGER,
                        Salary INT CHECK(Salary >= 0),
                        Experience INT CHECK(Experience >= 0),
-                       StartDate TEXT CHECK('1950-01-01'<= StartDate AND StartDate < DATE('now','+1 day')),
+                       StartDate TEXT CHECK('1950-01-01'<= StartDate AND StartDate < '2025-09-17'),
                        FOREIGN KEY (CompanyID) REFERENCES COMPANY(ID)
                    )
                    
@@ -44,7 +47,7 @@ def create_table_company(cursor):
                         CompanyName VARCHAR(50) NOT NULL UNIQUE,
                         CompanyVision TEXT NOT NULL,
                         CountryID INTEGER,
-                        EstablishedDate TEXT CHECK('1850-01-01'<= EstablishedDate AND EstablishedDate < DATE('now','+1 day')),
+                        EstablishedDate TEXT CHECK('1850-01-01'<= EstablishedDate AND EstablishedDate < '2025-09-17'),
                         FOREIGN KEY (CountryID) REFERENCES COUNTRY(ID)
                     )
                    
@@ -68,7 +71,7 @@ def create_tables(cursor):
 
 
 #%% ınsert some samples
-def insert_samples_to_country(cursor):
+def insert_samples_to_country(conn, cursor):
     countries = [
         ('Turkey',),
         ('Azerbaijan',),
@@ -105,38 +108,55 @@ def insert_samples_to_country(cursor):
     cursor.executemany(
         'INSERT OR IGNORE INTO COUNTRY(Country) VALUES (?)',countries,
     )
+    conn.commit()
     
+    country_ids = {}
+    for row in cursor.execute('SELECT ID, Country FROM COUNTRY').fetchall():
+        country_ids[row[1]] = row[0]
+    #country_ids = {row[1]: row[0] for row in cursor.execute('SELECT Country, ID FROM COUNTRY').fetchall()} #fetchall returns a list
+    return country_ids
     
-def insert_samples_to_company(cursor):
+def insert_samples_to_company(conn,cursor,country_ids):
     # CompanyName, CompanyVision, CountryID, EstablishedDate, 
     companies = [
     # Turkiye
-    ('ASTOR','To be a global brand representing our country by adopting innovative, continuous development as the most basic principle in the sector with the participation of all stakeholders.',1,'1983-07-29'),
-    ('Sabanci Group','We unite Türkiye and the World for a sustainable life with leading enterprises.',1,'1967-04-17'),
-    ('Koc Group','Lead.Together',1,'1923-05-31'),
-    ('Microsoft','to help people and businesses throughout the world realize their full potential',6, '1975-04-04'),
-    ('Nvidia','to solve problems that ordinary computers cannot',6, '1993-04-05'),
+    # Employee tablosu olusurken hata aldim
+    # ('ASTOR','To be a global brand representing our country by adopting innovative, continuous development as the most basic principle in the sector with the participation of all stakeholders.',1,'1983-07-29'),
+    # 
+    ('ASTOR','To be a global brand representing our country by adopting innovative, continuous development as the most basic principle in the sector with the participation of all stakeholders.',country_ids['Turkey'],'1983-07-29'),
+    ('Sabanci Group','We unite Türkiye and the World for a sustainable life with leading enterprises.',country_ids['Turkey'],'1967-04-17'),
+    ('Koc Group','Lead.Together',country_ids['Turkey'],'1923-05-31'),
+    ('Microsoft','to help people and businesses throughout the world realize their full potential',country_ids['USA'], '1975-04-04'),
+    ('Nvidia','to solve problems that ordinary computers cannot',country_ids['USA'], '1993-04-05'),
     # Azerbaycan
-    ('SOCAR','To be a leading integrated energy company in the Caspian region',2,'1992-09-13'),
-    ('Azercell','Connecting people across Azerbaijan with innovative telecommunication solutions',2,'1996-12-17'),
+    ('SOCAR','To be a leading integrated energy company in the Caspian region',country_ids['Azerbaijan'],'1992-09-13'),
+    ('Azercell','Connecting people across Azerbaijan with innovative telecommunication solutions',country_ids['Azerbaijan'],'1996-12-17'),
 
     # İngiltere
-    ('BP','To provide energy products and services safely and sustainably for people worldwide',14,'1909-04-01'),
-    ('Rolls-Royce','To deliver power systems that are reliable, efficient and sustainable',14,'1906-03-15'),
+    ('BP','To provide energy products and services safely and sustainably for people worldwide',country_ids['England'],'1909-04-01'),
+    ('Rolls-Royce','To deliver power systems that are reliable, efficient and sustainable',country_ids['England'],'1906-03-15'),
 
     # Almanya
-    ('Siemens','To be the global leader in electrification, automation and digitalization',7,'1847-10-12'),
-    ('Volkswagen','To create sustainable mobility for future generations',7,'1937-05-28'),
+    ('Siemens','To be the global leader in electrification, automation and digitalization',country_ids['Germany'],'1847-10-12'),
+    ('Volkswagen','To create sustainable mobility for future generations',country_ids['Germany'],'1937-05-28'),
 
     # İtalya
-    ('Fiat','To make innovative and stylish automobiles accessible to everyone',9,'1899-07-11'),
-    ('Ferrari','To deliver the most exclusive and exciting sports cars in the world',9,'1939-09-13')
+    ('Fiat','To make innovative and stylish automobiles accessible to everyone',country_ids['Italy'],'1899-07-11'),
+    ('Ferrari','To deliver the most exclusive and exciting sports cars in the world',country_ids['Italy'],'1939-09-13')
         
     ]
     cursor.executemany(
         'INSERT OR IGNORE INTO COMPANY(CompanyName, CompanyVision, CountryID, EstablishedDate) VALUES (?, ?, ?, ?)',companies
     )
-
+    conn.commit()
+    print("Inserted Companies:")
+    inserted_companies = cursor.execute('SELECT CompanyName FROM COMPANY').fetchall()
+    for company in inserted_companies:
+        print(company[0])
+        
+    company_ids = {row[1]: row[0] for row in cursor.execute('SELECT ID, CompanyName FROM COMPANY').fetchall()} # dict
+    
+    return company_ids
 '''
 1 - ASTOR  
 2 - Sabanci Group  
@@ -152,48 +172,51 @@ def insert_samples_to_company(cursor):
 12 - Fiat  
 13 - Ferrari  
 '''
-def insert_samples_to_employee(cursor):
+def insert_samples_to_employee(conn, cursor,company_ids):
     # Name, Surname, Email, CompanyID, Salary, Experience, StartDate
     employees = [
         # Türkiye (CompanyID: 1, 2, 3) => ASTOR, Sabanci Group, Koc Group
-        ('Emre','Ustubec','emre@gmail.com',1, 35000, 3, '2019-07-19'),
-        ('Erol','Ustubec','erol@gmail.com',1, 65000, 6, '2010-10-12'),
-        ('Sule','Ergin','sulergin@outlook.com',2, 25500, 0, '2022-11-18'),
-        ('Fatma','Senol','fatma@yahoo.com',2, 45000, 2, '2022-02-21'),
-        ('Emre','Dildoker','emredildoker@gmail.com',3, 75000, 6, '2019-03-30'),
-        ('Cemre','Suberker','csuberker@outlook.com',3, 225000, 15, '2010-03-30'),
-        ('Ahmet','Yilmaz','ahmet.yilmaz@gmail.com',1, 42000, 4, '2020-08-11'),
-        ('Ayse','Demir','ayse.demir@yahoo.com',2, 38000, 2, '2021-09-15'),
-        ('Mehmet','Kaya','mehmet.kaya@outlook.com',3, 56000, 7, '2015-04-25'),
+        ('Emre','Ustubec','emre@gmail.com',company_ids['ASTOR'], 35000, 3, '2019-07-19'),
+        ('Erol','Ustubec','erol@gmail.com',company_ids['ASTOR'], 65000, 6, '2010-10-12'),
+        ('Sule','Ergin','sulergin@outlook.com',company_ids['Sabanci Group'], 25500, 0, '2022-11-18'),
+        # ('Fatma','Senol','fatma@yahoo.com',2, 45000, 2, '2022-02-21'),
+        ('Fatma','Senol','fatma@yahoo.com',company_ids['Sabanci Group'], 45000, 2, '2022-02-21'),
+        
+        ('Emre','Dildoker','emredildoker@gmail.com',company_ids['Koc Group'], 75000, 6, '2019-03-30'),
+        ('Cemre','Suberker','csuberker@outlook.com',company_ids['Koc Group'], 225000, 15, '2010-03-30'),
+        ('Ahmet','Yilmaz','ahmet.yilmaz@gmail.com',company_ids['ASTOR'], 42000, 4, '2020-08-11'),
+        ('Ayse','Demir','ayse.demir@yahoo.com',company_ids['Sabanci Group'], 38000, 2, '2021-09-15'),
+        ('Mehmet','Kaya','mehmet.kaya@outlook.com',company_ids['Koc Group'], 56000, 7, '2015-04-25'),
 
         # USA (CompanyID: 4, 5) => Microsoft, Nvidia
-        ('John','Smith','john.smith@microsoft.com',4, 95000, 10, '2012-03-18'),
-        ('Zeynep','Canikli','caniklizeynek@yahoo.com',4, 27500, 2, '2023-11-24'),
-        ('Zuleyha','Kirmizigul','zuleyhakgul@outlook.com',5, 37500, 3, '2021-06-14'),
-        ('Emily','Johnson','emily.johnson@nvidia.com',5, 88000, 8, '2014-07-22'),
-        ('Polen','Hacioglu','hacioglupolen@gmail.com',6, 47500, 4, '2020-05-01'),
+        ('John','Smith','john.smith@microsoft.com',company_ids['Microsoft'], 95000, 10, '2012-03-18'),
+        ('Zeynep','Canikli','caniklizeynek@yahoo.com',company_ids['Microsoft'], 27500, 2, '2023-11-24'),
+        ('Zuleyha','Kirmizigul','zuleyhakgul@outlook.com',company_ids['Nvidia'], 37500, 3, '2021-06-14'),
+        ('Emily','Johnson','emily.johnson@nvidia.com',company_ids['Nvidia'], 88000, 8, '2014-07-22'),
 
         # Azerbaycan (CompanyID: 6, 7) => SOCAR, Azercell
-        ('Rashad','Aliyev','rashad.aliyev@socar.az',6, 52000, 5, '2018-06-30'),
-        ('Leyla','Mammadova','leyla.azercell@gmail.com',7, 41000, 3, '2020-12-05'),
+        ('Polen','Hacioglu','hacioglupolen@gmail.com',company_ids['SOCAR'], 47500, 4, '2020-05-01'),
+        ('Rashad','Aliyev','rashad.aliyev@socar.az',company_ids['SOCAR'], 52000, 5, '2018-06-30'),
+        ('Leyla','Mammadova','leyla.azercell@gmail.com',company_ids['Azercell'], 41000, 3, '2020-12-05'),
 
         # İngiltere (CompanyID: 8, 9) => BP, Rolls-Royce
-        ('Oliver','Brown','oliver.brown@bp.com',8, 78000, 9, '2013-11-19'),
-        ('Sophia','Taylor','sophia.taylor@rollsroyce.co.uk',9, 99000, 12, '2010-01-07'),
+        ('Oliver','Brown','oliver.brown@bp.com',company_ids['BP'], 78000, 9, '2013-11-19'),
+        ('Sophia','Taylor','sophia.taylor@rollsroyce.co.uk',company_ids['Rolls-Royce'], 99000, 12, '2010-01-07'),
 
         # Almanya (CompanyID: 10, 11) => Siemens - Volkswagen
-        ('Hans','Müller','hans.mueller@siemens.de',10, 87000, 11, '2011-09-28'),
-        ('Anna','Schmidt','anna.schmidt@vw.com',11, 65000, 6, '2016-05-14'),
+        # ('Hans','Muller','hans.mueller@siemens.de',company_ids['Siemens'], 87000, 11, '2011-09-28'), # Hata verdi
+        ('Anna','Schmidt','anna.schmidt@vw.com',company_ids['Volkswagen'], 65000, 6, '2016-05-14'),
 
         # İtalya (CompanyID: 12, 13) => Fiat, Ferrari
-        ('Marco','Rossi','marco.rossi@fiat.it',12, 54000, 5, '2017-08-02'),
-        ('Giulia','Bianchi','giulia.bianchi@ferrari.it',13, 125000, 14, '2009-10-11')
+        ('Marco','Rossi','marco.rossi@fiat.it',company_ids['Fiat'], 54000, 5, '2017-08-02'),
+        ('Giulia','Bianchi','giulia.bianchi@ferrari.it',company_ids['Ferrari'], 125000, 14, '2009-10-11')
         
     ]
     cursor.executemany('''
                    INSERT OR IGNORE INTO EMPLOYEE(Name, Surname, Email, CompanyID, Salary, Experience, StartDate) VALUES(?, ?, ?, ?, ?, ?, ?)
-                   
                    ''',employees)
+    conn.commit()
+    
     
 #%% create main function
 def main():
@@ -205,17 +228,21 @@ def main():
         conn.execute("PRAGMA foreign_keys = ON;")
         cursor = conn.cursor()
         print("Connection successful.") 
+        
         # call the function
         create_tables(cursor)
-        
+        print("Tables created successfully:)")   
         # insert some samples
-        insert_samples_to_country(cursor=cursor)
-        insert_samples_to_company(cursor)
-        insert_samples_to_employee(cursor)
         
+        country_ids = insert_samples_to_country(conn,cursor=cursor)
+        company_ids = insert_samples_to_company(conn,cursor,country_ids)
+                
+        insert_samples_to_employee(conn,cursor,company_ids)
+        print("All Data Inserted Successfully:)")
         # save required operations (islemleri kaydet)
         conn.commit()
-        print("Tables created successfully:)")     
+          
+
 #%% call the main() func
 if __name__ == "__main__":
     """conn, cursor = create_database()
